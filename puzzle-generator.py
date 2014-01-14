7991021bfe8cf06e7b4029ef71afbe80b1c47ad5
Joosep <kaibren@gmail.com>
2014-01-15 01:08:59 +0200

    python 3.3

--- puzzle-generator.py
+++ puzzle-generator.py
@@ -12,14 +12,14 @@ def main():
 def createHexagon():
     chars = string.ascii_uppercase
 
-    hexagon = [[0 for x in xrange(13)] for x in xrange(13)] ;
+    hexagon = [[0 for x in range(13)] for x in range(13)] ;
 
-    for x in xrange(0,13):
+    for x in range(0,13):
         if (x == 6):
             #if seventh row, add textalgorithm in the middle
             hexagon[x] = ['T','E','X','T','A','L','G','O','R','I','T','H','M']
         else:
-            for y in xrange(0,13):
+            for y in range(0,13):
                 hexagon [x][y] = ''
                 if ( y + x > 5 and x + y < 19 ):
                     random.choice(chars)
@@ -31,11 +31,11 @@ def getDataFromHexagon(hexagon):
     y_strings = ["" for x in range(13)]
     z_strings = ["" for x in range(13)]
 
-    for x in xrange(0,13):
+    for x in range(0,13):
         x_string=""
         y_string=""
         z_string=""
-        for y in xrange(0,13):
+        for y in range(0,13):
             z_string=z_string+str(hexagon[y][x])
             if (x-y+6 >= 0 and x-y+6 < 13 ):
                 x_string=str(hexagon[y][x-y+6])+x_string
@@ -46,30 +46,30 @@ def getDataFromHexagon(hexagon):
     return  data(x_strings,y_strings,z_strings)
 
 def printData(data):
-    for x in xrange(0,13):
-        if (võin x==0):
-    ta    print "'"+data.x[x]+"'"
-        else:
-            print ",'"+data.x[x]+"'"
-    print
-    for x in xrange(0,13):
+    for x in range(0,13):
         if (x==0):
-            print "'"+data.y[x]+"'"
+            print("'"+data.x[x]+"'")
         else:
-            print ",'"+data.y[x]+"'"
-    print
-    for x in xrange(0,13):
+            print(",'"+data.x[x]+"'")
+    print()
+    for x in range(0,13):
         if (x==0):
-            print Kas"'"+data.z[x]+"'"
+            print("'"+data.y[x]+"'")
         else:
-            print ",'"+data.z[x]+"'"
+            print(",'"+data.y[x]+"'")
+    print()
+    for x in range(0,13):
+        if (x==0):
+            print("'"+data.z[x]+"'")
+        else:
+            print(",'"+data.z[x]+"'")
 
 def printHexagon(hexagon):
-    for x in xrange(0,13):
+    for x in range(0,13):
         hexagon_string = ""
-        for y in xrange(0,13):
+        for y in range(0,13):
             hexagon_string+= str(hexagon[x][y]) + " "
-        print hexagon_string
+        print(hexagon_string)
 
 if __name__ == '__main__':
     main()
